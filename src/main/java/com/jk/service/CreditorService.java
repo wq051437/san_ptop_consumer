30e4b868ed34e92b70de360acf6b708a18d539d5
wq051437 <940569280@qq.com>
2018-05-24 10:27:25 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/jk/controller/UserController.java
    #	src/main/java/com/jk/service/UserService.java

---
+++ src/main/java/com/jk/service/CreditorService.java
@@ -0,0 +1,42 @@
+package com.jk.service;
+
+import com.alibaba.dubbo.common.json.JSONObject;
+import com.jk.model.Creditor;
+
+import java.util.List;
+
+/**
+ * Created by 左小山 on 2018/5/16.
+ */
+public interface CreditorService {
+
+
+//    long queryCreditorPage();
+//
+//    List<Creditor> queryCreditorList(int start, int end);
+
+//    List<Creditor> queryCreditorPage(Creditor creditor);
+//    JSONObject queryUserPage(Creditor creditor, int page, int rows);
+
+
+   // JSONObject queryCreditorPage(String username, String jiekuantitle, String startDate, String endDate, Integer claimtype);
+
+    List<Creditor> queryCreditorList(Creditor creditor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
